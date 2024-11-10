77c92a08e86a9059fb5265f4cc6257275d25343d
Aaqil Razak <aaqil.razak@gmail.com>
2024-11-10 03:46:23 -0600

    Added conversion for block to dictionary for Blockchain usage

--- Block.py
+++ Block.py
@@ -9,3 +9,12 @@ class Block:
         self.interactions = interactions
         self.proof = proof
         self.previous_hash = previous_hash
+# Similar to the interaction class where we convert the block into a format for Blockchain usage
+    def to_dict(self):
+        return {
+         'index': self.index,
+         'timestamp': self.timestamp,
+         'interactions': [interaction.to_dict() for interaction in self.interactions],
+         'proof': self.proof,
+         'previous_hash': self.previous_hash,
+      }
